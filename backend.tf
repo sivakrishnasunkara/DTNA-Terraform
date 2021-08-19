f95dba0477ea548a5afad645ea8abc10dfa0e7b1
sivakrishnasunkara <71445251+sivakrishnasunkara@users.noreply.github.com>
2021-08-19 20:02:23 +0530

    del backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "TestRG"
-    storage_account_name = "dtnaterraformsa"
-    container_name       = "terraform-container"
-    key                  = "terraform.tfstate"
-  }
-}
