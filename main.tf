175ba6c470b1952fe2f67dfcc87c29a2e2da3021
sivakrishnasunkara <71445251+sivakrishnasunkara@users.noreply.github.com>
2021-08-31 16:03:36 +0530

    updated df

--- main.tf
+++ main.tf
@@ -132,8 +132,8 @@ resource "azurerm_databricks_workspace" "bricks" {
 # }
 
 # creating data factory
-# resource "azurerm_data_factory" "dtnadatafactory" {
-#   name                = var.dtnadatafactory
-#   location            = azurerm_resource_group.RG1.location
-#   resource_group_name = azurerm_resource_group.RG1.name
-# }
+resource "azurerm_data_factory" "dtnadatafactory" {
+  name                = var.dtnadatafactory
+  location            = azurerm_resource_group.RG1.location
+  resource_group_name = azurerm_resource_group.RG1.name
+}
